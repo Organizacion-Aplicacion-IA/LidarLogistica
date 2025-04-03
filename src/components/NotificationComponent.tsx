3aa115907580da1a870d8821a5cc1ad2c0248edd
IrvingGeovanni1 <irving.carrillo.isw@unipolidgo.edu.mx>
2025-04-02 23:20:11 -0600

    🎨 Estilos de componentes de notificacion

--- src/components/NotificationComponent.tsx
+++ src/components/NotificationComponent.tsx
@@ -16,12 +16,15 @@ interface NotificationProps {
 
 const NotificationComponent: React.FC<NotificationProps> = ({title, descripcion}) => {
     return(
+    <div className="notificacionContenedor">
     <IonItem className="notification-item">
+        <div className="notification-rectangulo"></div>
         <IonLabel className="notification-text">
             <h2>{title}</h2>
             <p>{descripcion}</p>
         </IonLabel>
     </IonItem>
+    </div>
 
     );
     
